ca0c0e2d8b357a1bc8aa878f172e2dab7ceafd9c
Arthur <34348870+ArthurHoeke@users.noreply.github.com>
2026-01-21 10:09:54 +0100

    update app key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBQwSThqbXHqWu9-FRfcbJ9A2p1oTq7e5s',
-    authDomain: 'arm-mc.firebaseapp.com',
-    projectId: 'arm-mc',
-    storageBucket: 'arm-mc.firebasestorage.app',
-    messagingSenderId: '276292879066',
-    appId: '1:276292879066:android:53a1b69ce6ac66f43e1be3',
-    databaseURL: 'https://arm-mc-default-rtdb.europe-west1.firebasedatabase.app'
+    apiKey: "AIzaSyBFspm96MswCFodzHJeTvNZHDNB5jxiakQ",
+    authDomain: "arm-mc.firebaseapp.com",
+    projectId: "arm-mc",
+    databaseURL: "https://arm-mc-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "arm-mc.firebasestorage.app",
+    messagingSenderId: "276292879066",
+    appId: "1:276292879066:web:4a07cf2008f6713f3e1be3"
   },
 };
